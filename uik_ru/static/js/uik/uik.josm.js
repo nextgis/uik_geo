c3e73f9bb83ffdf9a89f59744eb54c5600e62f2e
vpavlenko <cxielamiko@gmail.com>
2013-07-13 12:42:58 +0400

    Added Jump-to-JOSM button

--- uik_ru/static/js/uik/uik.josm.js
+++ uik_ru/static/js/uik/uik.josm.js
@@ -16,7 +16,15 @@
         },
 
         bindEvents: function () {
-
+            $('#josm-link-img').on('mouseover', function() {
+                var bounds = UIK.viewmodel.map.getBounds();
+                var link = ('http://127.0.0.1:8111/load_and_zoom?' + 
+                        'left=' + bounds.getNorthWest().lng + 
+                        '&top=' + bounds.getNorthWest().lat + 
+                        '&right=' + bounds.getSouthEast().lng + 
+                        '&bottom=' + bounds.getSouthEast().lat);
+                $('#josm-link').attr('href', link);
+            });
         },
 
         setDomOptions: function () {
