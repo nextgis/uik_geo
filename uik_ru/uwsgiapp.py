dfef8e29db308c6395c754c5f3a58598e9f5aad0
Ivan Kavaliou <jownkiv@gmail.com>
2016-06-18 23:38:12 +0300

    added app to uwsgi support

---
+++ uik_ru/uwsgiapp.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import os
+
+from paste.deploy import loadapp
+if 'PASTE_CONFIG' in os.environ:
+    app = loadapp('config:' + os.environ['PASTE_CONFIG'])
+
+
+def application(environ, start_response):
+    if os.environ.get('WSGI_FILE_WRAPPER', 'yes').lower() in ('no', 'false'):
+        environ.pop('wsgi.file_wrapper', None)
+
+    return app(environ, start_response)
